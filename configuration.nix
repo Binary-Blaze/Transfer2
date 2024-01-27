316f24f3a5a5b8b1f90dd533ea0ea219c84eb469
Binary-Blaze <133932049+Binary-Blaze@users.noreply.github.com>
2024-01-27 10:58:02 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,17 @@
-{pkgs, user, ...}: {
-  
-  imports = [
-    ./hardware-configuration.nix
-  ];
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
-  documentation.nixos.enable = false;
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+    
+    boot.kernelPackages = pkgs.linuxPackages_zen;
+    documentation.nixos.enable = false;
 
   nix = {
     settings = {
@@ -28,55 +35,129 @@
     };
   };
 
-  networking = {
-    hostName = "nixos-studio";
-    networkmanager.enable = true;
-    enableIPv6 = false;
-    firewall.enable = false;
-  };
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+  
+  # Bootloader.
 
-  time.timeZone = "America/Chicago";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    packages = [pkgs.terminus_font];
-    font = "${pkgs.terminus_font}/share/consolefonts/ter-i22b.psf.gz";
-    useXkbConfig = true;
-  };
-
-  services = {
-    flatpak.enable = true;
-    dbus.enable = true;
-    picom.enable = true;
-
-    xserver = {
-      enable = true;
-      windowManager.dwm.enable = true;
-      layout = "us";
-
-      displayManager = {
-        lightdm.enable = true;
-        setupCommands = ''
-          ${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
-        '';
-        autoLogin = {
-          enable = true;
-          user = "jsampson";
-        };
+  
+  boot = {
+    kernelParams = ["nohibernate"];
+    tmp.cleanOnBoot = true;
+    supportedFilesystems = ["ntfs"];
+    loader = {
+      grub = {
+        device = "/dev/sda";
+        enable = true;
+        useOSProber = true;
+        timeoutStyle = "menu";
       };
+      timeout = 300;
+    };
+
+    kernelModules = ["tcp_bbr"];
+    kernel.sysctl = {
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.core.default_qdisc" = "fq";
+      "net.core.wmem_max" = 1073741824;
+      "net.core.rmem_max" = 1073741824;
+      "net.ipv4.tcp_rmem" = "4096 87380 1073741824";
+      "net.ipv4.tcp_wmem" = "4096 87380 1073741824";
     };
   };
+  
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  nixpkgs.overlays = [
-    (final: prev: {
-      dwm = prev.dwm.overrideAttrs (old: {src = /home/${user}/CTT-Nix/system/dwm-jsampson;}); #FIX ME: Update with path to your dwm folder
-    })
-  ];
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Detroit";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  virtualisation.libvirtd.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.flatpak.enable = true;
+  services.dbus.enable = true;
+  services.picom.enable = true;
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+  
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      	};
+      };
+    };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jsampson = {
     isNormalUser = true;
-    description = "jsampson";
-    extraGroups = [
+    description = "Jeremy M Sampson";
+    extraGroups = [ 
       "flatpak"
       "disk"
       "qemu"
@@ -90,64 +171,156 @@
       "libvirtd"
       "root"
     ];
-  };
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-
-  fonts = {
     packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      font-awesome
-      source-han-sans
-      source-han-sans-japanese
-      source-han-serif-japanese
-      (nerdfonts.override {fonts = ["Meslo"];})
-    ];
-    fontconfig = {
-      enable = true;
-      defaultFonts = {
-        monospace = ["Meslo LG M Regular Nerd Font Complete Mono"];
-        serif = ["Noto Serif" "Source Han Serif"];
-        sansSerif = ["Noto Sans" "Source Han Sans"];
-      };
-    };
+        
+    
+];
+	
+    #  thunderbird
+    
   };
-
+   
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  
+  spice
+  spice-gtk
+  spice-vdagent
+  open-vm-tools
+  guestfs-tools
+  virglrenderer
+  x11spice
+  docker-client
+    docker
+    docker-compose
+    docker-machine-kvm
+    docker-machine-kvm2
+    x11docker
+    podman
+    podman-desktop
+    skopeo
+    buildah
+    terminator
+    dotnet-sdk
+    vscode-with-extensions
+    vscodium
+    brave
+    firefox
+    htop
+    ranger
+      fish
+      vim
+      wget
+      w3m
+      dmenu
+      neofetch
+      neovim
+      starship
+      bat
+      bazecor
+      cargo
+      celluloid
+      chatterino2
+      clang-tools_9
+      dunst
+      efibootmgr
+      elinks
+      eww
+      feh
+      flameshot
+      flatpak
+      floorp
+      fontconfig
+      freetype
+      fuse-common
+      gcc
+      gimp
+      git
+      github-desktop
+      gnome.gnome-keyring
+      gnugrep
+      gnumake
+      gparted
+      gnugrep
+      grub2
+      hugo
+      kitty
+      libverto
+      luarocks
+      lxappearance
+      mangohud
+      neovim
+      nfs-utils
+      ninja
+      nodejs
+      nomacs
+      openssl
+      os-prober
+      nerdfonts
+      pavucontrol
+      picom
+      polkit_gnome
+      powershell
+      protonup-ng
+      python3Full
+      python.pkgs.pip
+      qemu
+      ripgrep
+      rofi
+      steam
+      steam-run
+      sxhkd
+      st
+      stdenv
+      synergy
+      swaycons
+      terminus-nerdfont
+      tldr
+      trash-cli
+      unzip
+      variety
+      virt-manager
+      xclip
+      xdg-desktop-portal-gtk
+      xfce.thunar
+      xorg.libX11
+      xorg.libX11.dev
+      xorg.libxcb
+      xorg.libXft
+      xorg.libXinerama
+      xorg.xinit
+      xorg.xinput
+      zoxide
   ];
 
-  virtualisation.libvirtd.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-  xdg.portal = {
-    enable = true;
-    config.common.default = "*";
-    extraPortals = [pkgs.xdg-desktop-portal-gtk];
-  };
+  # List services that you want to enable:
 
-  security.polkit.enable = true;
+  # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
 
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = ["graphical-session.target"];
-      wants = ["graphical-session.target"];
-      after = ["graphical-session.target"];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-  system.stateVersion = "23.11";
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
 }
